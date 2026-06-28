0d282a9afebb673811fa74aa255462a355d0adf2
Krzysztof Stolarczyk <biuro@alfatim.pl>
2026-06-28 04:38:36 +0000

    Etap 4 - decorators zakończone, przygotowanie do blueprintów

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, session, abort, send_from_directory, send_file, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
---
+++ services/folder_service.py
@@ -0,0 +1,24 @@
+from flask import (
+    session,
+    request,
+    redirect,
+    url_for
+)
+
+import os
+import shutil
+
+from utils.paths import get_user_base_folder
+from werkzeug.utils import secure_filename
+
+
+def create_folder():
+    folder_name = secure_filename(request.form.get("folder_name"))
+    current_path = request.form.get("current_path", "")
+
+    user_root = get_user_base_folder(session["username"])
+    new_folder = os.path.join(user_root, current_path, folder_name)
+
+    os.makedirs(new_folder, exist_ok=True)
+
+    return redirect(url_for("dashboard", path=current_path))
